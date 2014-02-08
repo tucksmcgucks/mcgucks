1a2d0deed70b4277f900633e068e90a5db69ea1f
tucksmcgucks <tucker.knutson@gmail.com>
2014-02-08 10:06:59 -0600

    Update bitcoinunits.cpp

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("MGC");
+    case mBTC: return QString("mMGC");
+    case uBTC: return QString::fromUtf8("μMGC");
     default: return QString("???");
     }
 }
@@ -46,8 +46,8 @@ QString BitcoinUnits::description(int unit)
     switch(unit)
     {
     case BTC: return QString("SmallChange");
-    case mBTC: return QString("Milli-SmallChange (1 / 1,000)");
-    case uBTC: return QString("Micro-SmallChange (1 / 1,000,000)");
+    case mBTC: return QString("Milli-McGucks (1 / 1,000)");
+    case uBTC: return QString("Micro-McGucks (1 / 1,000,000)");
     default: return QString("???");
     }
 }
